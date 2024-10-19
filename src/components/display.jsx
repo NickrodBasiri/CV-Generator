2d830d25468bd26021f4d307566b9af5c7cc486f
NickrodBasiri <basirinickrod@gmail.com>
2024-10-19 15:36:15 -0400

    Info inputs display on header, styling.

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -4,7 +4,13 @@ export default function Display({name, email, phone}) {
     return (
         <>
         <div id="CV">
-            <h1>{name} {email} {phone}</h1>
+            <div id="header">
+                <h1>{name}</h1>
+                <div id="contact-info">
+                    <h1>{email}</h1>
+                    <h1>{phone}</h1>
+                </div>
+            </div>
         </div>
         </>
     )
